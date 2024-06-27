27e606ab8452871cc3e31684f290b74baff101cd
toochi-ai <mikeilov.ea@gmail.com>
2024-06-27 18:06:59 +0300

    initial

---
+++ 09_modul/static_methods.py
@@ -0,0 +1,28 @@
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+
+class SquareFactory:
+    @staticmethod
+    def create_square(side):
+        return Square(side)
+
+
+print(SquareFactory.create_square(50).side)
+
+
+class MathUtils:
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def multiply(x, y):
+        return x * y
+
+
+result_1 = MathUtils.add(5, 3)
+result_2 = MathUtils.multiply(4, 6)
+
+print(result_1, result_2)
