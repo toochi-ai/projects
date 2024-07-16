e6f48b051c45f9a3c227170a68038e1d52339564
toochi-ai <mikeilov.ea@gmail.com>
2024-07-17 01:23:14 +0300

    собственная структура данных

--- 11_modul/unit_5_tree.py
+++ 11_modul/unit_5_tree.py
@@ -68,3 +68,95 @@ print('---')
 node_root.post_order()
 print('---')
 node_root.in_order()
+
+print('-----')
+
+
+# ___________________________________________________________
+# СОБСТВЕННАЯ СТРУКТУРА ДАННЫХ, РЕАЛИЗУЮЩАЯ СВЯЗАННЫЙ СПИСОК
+# -----------------------------------------------------------
+
+class Node:  # класс элемента
+    def __init__(self, value=None, next_=None):  # инициализируем
+        self.value = value  # значением
+        self.next = next_  # и ссылкой на следующий элемент
+
+    def __str__(self):
+        return "Node value = " + str(self.value)
+
+
+class LinkedList:  # класс списка
+    def __init__(self):  # инициализируем пустым
+        self.first = None
+        self.last = None
+
+    def clear(self):  # очищаем список
+        self.__init__()
+
+    def __str__(self):  # функция печати
+        R = ''
+
+        pointer = self.first  # берём первый указатель
+        while pointer is not None:  # пока указатель не станет None
+            R += str(pointer.value)  # добавляем значение в строку
+            pointer = pointer.next  # идём дальше по указателю
+            if pointer is not None:  # если он существует, добавляем пробел
+                R += ' '
+        return R
+
+    def pushleft(self, value):
+        if self.first is None:
+            self.first = Node(value)
+            self.last = self.first
+        else:
+            new_node = Node(value, self.first)
+            self.first = new_node
+
+    def pushright(self, value):
+        if self.first is None:
+            self.first = Node(value)
+            self.last = self.first
+        else:
+            new_node = Node(value, self.first)
+            self.first = new_node
+
+    def popleft(self):
+        if self.first is None:  # если список пустой, возвращаем None
+            return None
+        elif self.first == self.last:  # если список содержит только один элемент
+            node = self.first  # сохраняем его
+            self.__init__()  # очищаем
+            return node  # и возвращаем сохранённый элемент
+        else:
+            node = self.first  # сохраняем первый элемент
+            self.first = self.first.next  # меняем указатель на первый элемент
+            return node  # возвращаем сохранённый
+
+    def popright(self):
+        if self.first is None:  # если список пустой, возвращаем None
+            return None
+        elif self.first == self.last:  # если список содержит только один элемент
+            node = self.first  # сохраняем его
+            self.__init__()  # очищаем
+            return node  # и возвращаем сохраненный элемент
+        else:
+            node = self.last  # сохраняем последний
+            pointer = self.first  # создаём указатель
+            while pointer.next is not node:  # пока не найдём предпоследний
+                pointer = pointer.next
+            pointer.next = None  # обнуляем указатели, чтобы
+            self.last = pointer  # предпоследний стал последним
+            return node  # возвращаем сохранённый
+
+
+LL = LinkedList()
+
+LL.pushright(1)
+LL.pushleft(2)
+LL.pushright(3)
+LL.popright()
+LL.pushleft(4)
+LL.pushright(5)
+LL.popleft()
+
+print(LL)
