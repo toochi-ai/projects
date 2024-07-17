1a5efc2b2f75cc68a8a4f423d3ed2f7b8f205f8d
toochi-ai <mikeilov.ea@gmail.com>
2024-07-17 23:44:18 +0300

    HTML и JSON-ответ

---
+++ 12_modul/Requests_JSON.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')  # попробуем поймать json-ответ
+texts = json.loads(r.content)  # делаем из полученных байтов Python-объект для удобной работы
+
+print(type(texts))  # проверяем тип сконвертированных данных
+
+for text in texts:  # Выводим полученный текст. Но для того чтобы он влез в консоль,
+    # оставим только первые 50 символов.
+    print(text[:50], '\n')
+
+print('---')
+
+r = requests.get('https://api.github.com')
+d = json.loads(r.content)  # делаем из полученных байтов Python-объект для удобной работы
+print(d['following_url'])  # обращаемся к полученному объекту как к словарю и
+# попробуем напечатать одно из его значений
