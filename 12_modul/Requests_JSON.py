e593171a8832854f5971c45ac909f1860c9919cf
toochi-ai <mikeilov.ea@gmail.com>
2024-07-17 23:48:06 +0300

    POST-запрос

--- 12_modul/Requests_JSON.py
+++ 12_modul/Requests_JSON.py
@@ -1,4 +1,5 @@
 import json
+
 import requests
 
 r = requests.get('https://baconipsum.com/api/?type=meat-and-filler')  # попробуем поймать json-ответ
@@ -16,3 +17,12 @@ r = requests.get('https://api.github.com')
 d = json.loads(r.content)  # делаем из полученных байтов Python-объект для удобной работы
 print(d['following_url'])  # обращаемся к полученному объекту как к словарю и
 # попробуем напечатать одно из его значений
+
+print('---')
+# POST-запрос
+# --------------
+
+r = requests.post('https://httpbin.org/post', data={'key': 'value'})  # отправляем POST-запрос
+
+print(r.content)  # содержимое ответа и его обработка происходит так же,
+# как и с GET-запросами, разницы никакой нет
