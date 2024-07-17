0af34478557a82392ec6f46cd63884a802f89e32
toochi-ai <mikeilov.ea@gmail.com>
2024-07-17 22:14:01 +0300

    сортировка слиянием

--- 11_modul/unit_8.py
+++ 11_modul/unit_8.py
@@ -38,3 +38,38 @@ for i in range(1, len(array)):
 
 print(count)
 
+
+# Сортировка слиянием
+# --------------------
+
+def merge_sort(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L) // 2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
