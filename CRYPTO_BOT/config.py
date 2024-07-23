de3d56686c70f57769545d6fde8799d5f3f1f4c6
toochi-ai <mikeilov.ea@gmail.com>
2024-07-23 23:06:04 +0300

    добавил валюту

--- CRYPTO_BOT/config.py
+++ CRYPTO_BOT/config.py
@@ -3,5 +3,7 @@ TOKEN = '7471955957:AAFhXEaDb9v3_ZptX1PE-WtdFBu5yteoOIw'
 keys = {
     'биткоин': 'BTC',
     'эфириум': 'ETH',
-    'доллар': 'USD'
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'фунт': 'GBP'
 }
