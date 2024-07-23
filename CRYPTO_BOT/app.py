067ba6873644861ad1d8ef9b45a79e8e012d58ca
toochi-ai <mikeilov.ea@gmail.com>
2024-07-23 22:41:50 +0300

    рефакторинг, добавил config.py и utils.py

--- CRYPTO_BOT/app.py
+++ CRYPTO_BOT/app.py
@@ -1,21 +1,9 @@
-import json
-
-import requests
 import telebot
 
-TOKEN = '7471955957:AAFhXEaDb9v3_ZptX1PE-WtdFBu5yteoOIw'
+from config import keys, TOKEN
+from utils import ConvertionException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
-
-keys = {
-    'биткоин': 'BTC',
-    'эфириум': 'ETH',
-    'доллар': 'USD'
-}
-
-
-class ConvertionException(Exception):
-    pass
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -37,31 +25,12 @@ def values(message: telebot.types.Message):
 def convert(message: telebot.types.Message):
     values = message.text.split(' ')
 
-    if len(values) > 3:
+    if len(values) != 3:
         raise ConvertionException('Слишком много параметров.')
 
     quote, base, amount = values
+    total_base = CryptoConverter.convert(quote, base, amount)
 
-    if quote == base:
-        raise ConvertionException(f'Невозможно конвертировать одинаковые валюты {base}.')
-
-    try:
-        quote_ticker = keys[quote]
-    except KeyError:
-        raise ConvertionException(f'Не удалось выбрать валюту {quote}')
-
-    try:
-        base_ticker = keys[base]
-    except KeyError:
-        raise ConvertionException(f'Не удалось выбрать валюту {base}')
-
-    try:
-        amount = float(amount)
-    except ValueError:
-        raise ConvertionException(f'Не удалось обработать количество {amount}.')
-
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-    total_base = json.loads(r.content)[keys[base]]
     text = f'Цена {amount} {quote} в {base} - {total_base}'
     bot.send_message(message.chat.id, text)
 
