d863c3db92670df74cc8aa0674e28aa110da8473
toochi-ai <mikeilov.ea@gmail.com>
2024-07-23 21:40:33 +0300

    функция convert, API

--- CRYPTO_BOT/app.py
+++ CRYPTO_BOT/app.py
@@ -1,3 +1,6 @@
+import json
+
+import requests
 import telebot
 
 TOKEN = '7471955957:AAFhXEaDb9v3_ZptX1PE-WtdFBu5yteoOIw'
@@ -22,8 +25,17 @@ def help(message: telebot.types.Message):
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
-        text = '\n'.join((text, key, ))
+        text = '\n'.join((text, key,))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    quote, base, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {quote} в {base} - {total_base}'
+    bot.send_message(message.chat.id, text)
 
 
 bot.polling()
