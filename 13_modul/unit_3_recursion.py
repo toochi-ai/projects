122b566fb41d61ee20383eb055031f03bd23b97f
toochi-ai <mikeilov.ea@gmail.com>
2024-07-31 00:15:11 +0300

    рекурсия в работе с деревьями

--- 13_modul/unit_3_recursion.py
+++ 13_modul/unit_3_recursion.py
@@ -16,4 +16,63 @@ def factorial(n):
         return n * factorial(n - 1)
 
 
-print(factorial(5))
+print(f'факториал: {factorial(5)}')
+
+print('---')
+
+
+#  Дерево реализовано как вложенные списки. Каждый узел
+#  представляет собой список, состоящий из двух
+#  элементов — значения узла и списка его детей:
+class Node:
+    def __init__(self, value, children=None):
+        self.value = value
+        self.children = children if children else []
+
+
+root = Node(5, [
+    Node(3, [
+        Node(1),
+        Node(4)
+    ]),
+    Node(2, [
+        Node(6),
+        Node(-1)
+    ])
+])
+
+# Получение значения в узле-корне
+print(f'корень дерева: {root.value}')
+# Получение значений дочерних узлов
+print(f'дети корня: {root.children[0].value, root.children[1].value}')
+# Получение значений листьев
+print(f'листья дерева: {root.children[0].children[0].value, root.children[0].children[1].value}')
+print(f'листья дерева: {root.children[1].children[0].value, root.children[1].children[1].value}')
+
+
+#  Применяем рекурсию:
+def test_tree_structure(node):
+    # у node есть  value — значение
+    # и children — список других node
+
+    # Базовый случай
+    if node.value < 0:
+        return False
+
+    # Рекурсия
+    for child in node.children:
+        if not test_tree_structure(child):
+            return False
+
+    return True
+
+
+print('---')
+#  Итог работы функции: если не было найдено ни одного отрицательного узла,
+#  она вернёт True, в противном случае — False:
+
+# Тестируем наше дерево
+if test_tree_structure(root) is not True:
+    print(f'{test_tree_structure(root)} - в дереве присутствует отрицательный узел')
+else:
+    print(f'{test_tree_structure(root)} - в дереве отсутствует отрицательный узел')
