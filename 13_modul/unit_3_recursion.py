853caf131ebc9df1d84119743f5cb268a8a3ef00
toochi-ai <mikeilov.ea@gmail.com>
2024-07-30 22:56:59 +0300

    вычисление факториала

--- 13_modul/unit_3_recursion.py
+++ 13_modul/unit_3_recursion.py
@@ -7,3 +7,13 @@ def recursive_func(n=0):
 
 recursive_func()
 print('---')
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+print(factorial(5))
