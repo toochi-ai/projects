509b948132f1ee46769f581b5894a410a7345f72
toochi-ai <mikeilov.ea@gmail.com>
2024-08-07 15:58:22 +0300

    универсальный декоратор

--- 13_modul/unit_7_decorator.py
+++ 13_modul/unit_7_decorator.py
@@ -133,3 +133,32 @@ def power_sum(n, p):
 result = power_sum(10000, 2)
 
 print(result)
+print('---')
+
+#  универсальный декоратор
+
+
+def retry_on_failure(func):
+    max_retries = 3
+
+    def wrapper(*args, **kwargs):
+        for _ in range(max_retries):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                print(f"Function {func.__name__} failed with error: {e}. Retrying...")
+                time.sleep(1)  # В реальном кейсе нам чаще всего надо будет подождать нек-ое кол-во времени
+        print(f"Function {func.__name__} failed after {max_retries} retries.")
+
+    return wrapper
+
+
+@retry_on_failure
+def fragile_function(*args, **kwargs):
+    if random.random() < 0.5:  # В 50% случаев функция будет "Ломаться"
+        raise Exception("An error occurred")
+    else:
+        return "Success"
+
+
+print(fragile_function())
