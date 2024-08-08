76b22d8084baecf875ce566109f3bcb6ab8c87c6
toochi-ai <mikeilov.ea@gmail.com>
2024-08-08 21:36:11 +0300

    вывел данные в словаре import json

--- modul_14_Парсинг_данных/unit_2_data _parsing.py
+++ modul_14_Парсинг_данных/unit_2_data _parsing.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 response = requests.get(
     'https://catfact.ninja/fact',
@@ -9,3 +10,18 @@ response = requests.get(
 
 print(response.text)
 print(type(response.text))
+print('---')
+
+response = requests.get(
+    'https://catfact.ninja/fact',
+    headers={
+        'accept': 'application/xml'
+    }
+)
+
+response_text = json.loads(response.text)
+print(response_text)
+print(type(response_text))
+print('-----')
+print(response_text['fact'])
+print(response_text.keys())
