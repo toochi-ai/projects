56cd4a3d7bc0bbe206bf371b2b1d05f806d60dad
toochi-ai <mikeilov.ea@gmail.com>
2024-08-11 21:26:40 +0300

    вывел данные в словаре import json

--- modul_14_Парсинг_данных/unit_2_data _parsing.py
+++ modul_14_Парсинг_данных/unit_2_data _parsing.py
@@ -1,5 +1,7 @@
-import requests
 import json
+
+import requests
+import xmltodict
 
 response = requests.get(
     'https://catfact.ninja/fact',
@@ -25,3 +27,25 @@ print(type(response_text))
 print('-----')
 print(response_text['fact'])
 print(response_text.keys())
+print('---')
+
+car_data = """<car>
+               <brand>Kia</brand>
+               <color>White</color>
+               <engineVolume>1.6</engineVolume>
+               <name>Rio</name>
+               </car>"""
+parsed_data = xmltodict.parse(car_data)
+print(parsed_data)
+print('---')
+
+car_data = """brand Kia
+color white
+engine volume 1.6
+name Rio"""
+parsed_data = {}
+for row in car_data.split('\n'):
+    row = row.rsplit(' ', 1)
+    parsed_data[row[0]] = row[1]
+print(parsed_data)
+print('---')
