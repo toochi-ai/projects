114f7318cf8a6b1e47df24c473e3c8110fdfa56e
toochi-ai <mikeilov.ea@gmail.com>
2024-08-11 22:37:37 +0300

    переписал данные из одного файла в другой

--- modul_14_Парсинг_данных/unit_3_JSON_format.py
+++ modul_14_Парсинг_данных/unit_3_JSON_format.py
@@ -1,5 +1,24 @@
 import json
 
+with open('cars.json', 'r') as cars_file:  # Открыл файл
+    cars_data = cars_file.read()  # Прочитали файл
+    cars_data = json.loads(cars_data)  # Десериализовали данные
+
+response_data = []
+for car_item in cars_data:
+    response_data.append({
+        'car_name': f"{car_item.get('brand', '')} {car_item.get('name', '')}",
+        'transmission': car_item.get('transmission'),
+        'engine_volume': car_item.get('engine', {}).get('volume'),
+    })
+
+with open('cars_restructured.json', 'w') as cars_file:
+    response_data = json.dumps(response_data, indent=4)
+    cars_file.write(response_data)
+
+
+print(response_data)
+
 cyrillic_data = {
     'Ключ': 'значение',
 }
