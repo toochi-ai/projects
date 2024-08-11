606cd93e03cfaa1d4aa390646aa725feea6f7274
toochi-ai <mikeilov.ea@gmail.com>
2024-08-12 01:08:29 +0300

    воссоздать документ XML

--- modul_14_Парсинг_данных/unit_4_XML_format.py
+++ modul_14_Парсинг_данных/unit_4_XML_format.py
@@ -33,3 +33,36 @@ animals_dict = {
 with open('animals_new.xml', 'w') as animals_file:
     parsed_data = xmltodict.unparse(animals_dict, pretty=True)
     animals_data = animals_file.write(parsed_data)
+
+
+animals_dict = {
+   'cat': {
+           'name': 'Boris',
+           'breed': 'Siberian',
+           'owner': 'Oleg',
+           'age': '4'
+       },
+   'dog': {
+           'name': 'Chorizo',
+
+           'breed': 'Dachshund',
+           'owner': 'Elena',
+           'age': '3'
+       },
+   'hamster': {
+           'name': 'Arkadiy',
+           'breed': 'Dzungarian',
+           'owner': 'Elena',
+           'age': '1'
+   }
+}
+
+root = ET.Element('animals', attrib={'home_id': '822494'})
+for element, options in animals_dict.items():
+    an_element = ET.SubElement(root, element)
+    for key, option in options.items():
+        opt_element = ET.SubElement(an_element, key)
+        opt_element.text = option
+
+tree = ET.ElementTree(root)
+tree.write("animals_new_1.xml", encoding='unicode')
