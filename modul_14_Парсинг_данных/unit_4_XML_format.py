ae522cbb510456414be595257344959a2962a8eb
toochi-ai <mikeilov.ea@gmail.com>
2024-08-13 11:15:28 +0300

    добавил в файл squirrel

--- modul_14_Парсинг_данных/unit_4_XML_format.py
+++ modul_14_Парсинг_данных/unit_4_XML_format.py
@@ -65,8 +65,20 @@ for element, options in animals_dict.items():
         opt_element.text = option
 
 root[0][0].text = 'Basil'
-
 tree = ET.ElementTree(root)
+ET.indent(tree)
+tree.write("animals_new_1.xml", encoding='unicode')
+
+
+sq = ET.SubElement(root, 'squirrel')
+for k, v in {
+    'name': 'Batareyka',
+    'breed': 'Aberti',
+    'owner': 'Dima',
+    'age': '1'
+}.items():
+    opt = ET.SubElement(sq, k)
+    opt.text = v
 ET.indent(tree)
 tree.write("animals_new_1.xml", encoding='unicode')
 
