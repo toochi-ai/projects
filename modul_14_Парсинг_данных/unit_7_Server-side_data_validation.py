abddd8df720d3de3da8a4a0e1b2dd8192a645f72
toochi-ai <mikeilov.ea@gmail.com>
2024-08-16 23:49:07 +0300

    решил задание валидация данных в формате JSON

--- modul_14_Парсинг_данных/unit_7_Server-side_data_validation.py
+++ modul_14_Парсинг_данных/unit_7_Server-side_data_validation.py
@@ -24,3 +24,45 @@ print(validator.is_valid(json_data))
 
 for error in sorted(validator.iter_errors(json_data), key=str):
     print(error.message)
+print('---')
+
+schema = {
+    'type': 'array',
+    'prefixItems': [
+        {
+            'type': 'object',
+            'required': [
+                'room_id', 'room', 'price', 'category', 'extras'
+            ],
+            'properties': {
+                'room_id': {'type': 'integer'},
+                'room': {'type': 'string'},
+                'price': {'type': 'integer'},
+                'category': {'type': 'string'},
+                'extras': {
+                    'type': 'object',
+                    'required': [
+                        'meals', 'services'
+                    ],
+                    'properties': {
+                        'meals': {
+                            'type': 'array',
+                            'prefixItems': [
+                                {'type': 'integer'}
+                            ]
+                        },
+                        'services': {
+                            'type': 'array',
+                            'prefixItems': [
+                                {'type': 'integer'}
+                            ]
+                        }
+                    }
+                },
+            }
+        },
+    ],
+}
+
+validator = Draft202012Validator(schema)
+print(validator.is_valid(json_data))
