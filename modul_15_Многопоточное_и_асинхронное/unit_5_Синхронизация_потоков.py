bc9d87603b53547fc7c97f6cc32420f48ae9869e
toochi-ai <mikeilov.ea@gmail.com>
2024-08-18 20:11:47 +0300

    состоянием гонки (race condition)

---
+++ modul_15_Многопоточное_и_асинхронное/unit_5_Синхронизация_потоков.py
@@ -0,0 +1,29 @@
+import threading
+
+# общая переменная
+gX = 0
+
+
+# функция, которая увеличивает на 1 gX 100000 раз
+def threadFunc():
+    print('thread started')
+    global gX
+    for i in range(100000):
+        gX = gX + 1
+
+
+if __name__ == "__main__":
+    gX = 0
+
+    # запускает выполнение функции в пяти потоках
+threads = []
+for i in range(10):
+    threads.append(threading.Thread(target=threadFunc, args=()))
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+print(gX)
