d4139f0ce55138bbd333a23ed7542c08e0b51137
toochi-ai <mikeilov.ea@gmail.com>
2024-08-18 20:28:12 +0300

    Локи (Locks, блокировки)

--- modul_15_Многопоточное_и_асинхронное/unit_5_Синхронизация_потоков.py
+++ modul_15_Многопоточное_и_асинхронное/unit_5_Синхронизация_потоков.py
@@ -1,29 +1,33 @@
 import threading
 
-# общая переменная
 gX = 0
 
 
-# функция, которая увеличивает на 1 gX 100000 раз
-def threadFunc():
+def threadFunc(lock):
     print('thread started')
     global gX
     for i in range(100000):
+        # захватываем лок, делая все операции далее монопольными
+        lock.acquire()
         gX = gX + 1
+        # освобождаем лок
+        lock.release()
 
 
 if __name__ == "__main__":
     gX = 0
 
-    # запускает выполнение функции в пяти потоках
-threads = []
-for i in range(10):
-    threads.append(threading.Thread(target=threadFunc, args=()))
+    threads = []
+    # создаём лок
+    lock = threading.Lock()
+    for i in range(5):
+        # и передаём его в качестве аргумента в каждый поток
+        threads.append(threading.Thread(target=threadFunc, args=(lock)))
 
-for thread in threads:
-    thread.start()
+    for thread in threads:
+        thread.start()
 
-for thread in threads:
-    thread.join()
+    for thread in threads:
+        thread.join()
 
-print(gX)
+    print(gX)
