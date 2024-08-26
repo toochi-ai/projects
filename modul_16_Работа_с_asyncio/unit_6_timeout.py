3ad5673117592acdaf5caff5fe140193d88cab83
toochi-ai <mikeilov.ea@gmail.com>
2024-08-26 15:51:53 +0300

    добавил пример с использованием wait

--- modul_16_Работа_с_asyncio/unit_6_timeout.py
+++ modul_16_Работа_с_asyncio/unit_6_timeout.py
@@ -1,5 +1,5 @@
-import time
 import asyncio
+import time
 
 
 async def some_long_async_task():
@@ -27,3 +27,35 @@ if __name__ == '__main__':
     tm = time.time()
     asyncio.run(main())
     print(f'Общее время работы {time.time() - tm}')
+print('---')
+
+
+async def some_async_task(timeout):
+    try:
+        print('Длительная задача начата')
+        await asyncio.sleep(timeout)
+        print('Длительная задача завершена')
+    except asyncio.exceptions.CancelledError:
+        print('Длительная задача прервана')
+
+
+async def main():
+    try:
+        done, undone = await asyncio.wait(
+            [some_async_task(0.5), some_async_task(1.0), some_async_task(5), some_async_task(10)], timeout=2.0)
+        print(f'Задачи готовы {[t.get_name() for t in done]}')
+        print(f'Задачи не готовы {[t.get_name() for t in undone]}')
+    except asyncio.exceptions.TimeoutError:
+        print('Таймаут выполнения задачи')
+    except Exception as e:
+        print(f'Exception: {e}')
+    except asyncio.exceptions.CancelledError as e:
+        print(f'Задача прервана')
+    print('Завершение выполнения асинхронных задач')
+
+
+if __name__ == "__main__":
+    tm = time.time()
+    asyncio.run(main())
+    print(f'Общее время работы {time.time() - tm}')
+print('---')
