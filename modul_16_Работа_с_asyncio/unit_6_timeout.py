5121d64c9f96fcb4241c92997f997da6d3012168
toochi-ai <mikeilov.ea@gmail.com>
2024-08-26 14:45:14 +0300

    задачи с timeout добавил метод shield с параметром timeout

--- modul_16_Работа_с_asyncio/unit_6_timeout.py
+++ modul_16_Работа_с_asyncio/unit_6_timeout.py
@@ -13,7 +13,11 @@ async def some_long_async_task():
 
 async def main():
     try:
-        await asyncio.wait_for(some_long_async_task(), 2.0)
+        await asyncio.wait_for(asyncio.shield(some_long_async_task()), 2.0)
+    except asyncio.exceptions.TimeoutError:
+        print('Таймаут выполнения задачи')
+    except Exception as e:
+        print(f'Exception: {e}')
     except asyncio.exceptions.CancelledError as e:
         print(f'Задача прервана')
     print('Завершение выполнения асинхронных задач')
