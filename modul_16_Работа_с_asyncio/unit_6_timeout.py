2557a466b15f34cc1e3cf6855e38a5bc548775fd
toochi-ai <mikeilov.ea@gmail.com>
2024-08-26 16:06:46 +0300

    добавил пример с использованием as_completed

--- modul_16_Работа_с_asyncio/unit_6_timeout.py
+++ modul_16_Работа_с_asyncio/unit_6_timeout.py
@@ -1,7 +1,6 @@
 import asyncio
 import time
 
-
 async def some_long_async_task():
     try:
         print('Длительная задача начата')
@@ -45,6 +44,40 @@ async def main():
             [some_async_task(0.5), some_async_task(1.0), some_async_task(5), some_async_task(10)], timeout=2.0)
         print(f'Задачи готовы {[t.get_name() for t in done]}')
         print(f'Задачи не готовы {[t.get_name() for t in undone]}')
+    except asyncio.exceptions.TimeoutError:
+        print('Таймаут выполнения задачи')
+    except Exception as e:
+        print(f'Exception: {e}')
+    except asyncio.exceptions.CancelledError as e:
+        print(f'Задача прервана')
+    print('Завершение выполнения асинхронных задач')
+
+
+if __name__ == "__main__":
+    tm = time.time()
+    asyncio.run(main())
+    print(f'Общее время работы {time.time() - tm}')
+print('---')
+
+
+async def some_async_task(timeout):
+    try:
+        print('Длительная задача начата')
+        await asyncio.sleep(timeout)
+        print('Длительная задача завершена')
+    except asyncio.exceptions.CancelledError:
+        print('Длительная задача прервана')
+        return None
+    return f'Результат за {timeout} секунд'
+
+
+async def main():
+    try:
+        # перебираем все задачи начиная с тех которые выполняются раньше
+        for task in asyncio.as_completed(
+                [some_async_task(0.5), some_async_task(1.0), some_async_task(5), some_async_task(10)], timeout=2.0):
+            earliest_result = await task
+            print(earliest_result)
     except asyncio.exceptions.TimeoutError:
         print('Таймаут выполнения задачи')
     except Exception as e:
