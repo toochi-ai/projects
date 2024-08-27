a021b30e3385307277e9d07e71d72d6d18f41421
toochi-ai <mikeilov.ea@gmail.com>
2024-08-27 21:05:08 +0300

    Асинхронные запросы на AIOHTTP

--- modul_17_асинхронные_вызовы_к_внешнему_API/unit_2.1_Асинхронные_HTTP-запросы.py
+++ modul_17_асинхронные_вызовы_к_внешнему_API/unit_2.1_Асинхронные_HTTP-запросы.py
@@ -1,6 +1,7 @@
 import asyncio
-import aiohttp
 import json
+import time
+import aiohttp
 
 token = 'cur_live_e3prRazEr7BLmmfM9P5kVgQl3ftxs1rob8Xt2bmJ'
 url = 'https://api.currencyapi.com/v3/latest'
@@ -14,3 +15,16 @@ async def curr_requests():
 
 currencies = asyncio.run(curr_requests())
 print(json.dumps(currencies, indent=4, ensure_ascii=False))
+print('---')
+
+
+async def curr_requests():
+    async with aiohttp.ClientSession() as session:
+        for _ in range(5):
+            async with session.get(url=f"{url}", headers={'apikey': token}) as resp:
+                print(resp.status)
+
+
+t = time.time()
+currencies = asyncio.run(curr_requests())
+print(f'Времени прошло {time.time() - t}')
