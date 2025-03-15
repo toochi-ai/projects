99b27d8b325240ba2e9332789c23124731672a3c
toochi-ai <mikeilov.ea@gmail.com>
2025-03-15 20:02:27 +0300

    all

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 # Импортируем созданные нами представления
-from .views import PostsList, PostDetail
+from .views import PostsList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
    # path — означает путь.
@@ -8,8 +8,13 @@ urlpatterns = [
    # Т.к. наше объявленное представление является классом,
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
-   path('', PostsList.as_view()),
+   path('', PostsList.as_view(),name='post_list'),
    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
    # int — указывает на то, что принимаются только целочисленные значения
-   path('<int:pk>', PostDetail.as_view()),
+   path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+   path('search/', PostsList.as_view(), name='post_filter'),
+   path('news/create/', PostCreate.as_view(), name='news_create'),
+   path('articles/create/', PostCreate.as_view(), name='articles_create'),
+   path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
+   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
 ]
